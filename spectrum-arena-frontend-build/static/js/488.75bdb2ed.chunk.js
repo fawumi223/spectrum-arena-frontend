0acd4f303acf5a38d0f8b8e7948e1f189b565cb6
APPL <appl@APPLs-MacBook-Air.local>
2026-04-04 13:53:06 +0100

    fix: clean repo and update gitignore

--- spectrum-arena-frontend-build/static/js/488.75bdb2ed.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkspectrum_arena_frontend=self.webpackChunkspectrum_arena_frontend||[]).push([[488],{488(e,n,a){new WeakMap;new Set}}]);
-//# sourceMappingURL=488.75bdb2ed.chunk.js.map
